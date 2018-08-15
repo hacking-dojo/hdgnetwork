b8d6826848a80b0ef8ed72cb0091856cd61e3251
Matthieu Berjon <matthieu@berjon.net>
2018-08-15 22:38:29 +0200

    Updating the CLI
    I updated the CLI in order to create:
    
    - a main command that can display the main help
    - a subcommand called `whois` dedicated to the whois tool
    
    The main command accepts a `version` option to show the current version
    of the project and the Python version installed on the machine.
    
    The `whois` subcommand make a simple test on the data to see if it's an
    IP address. If true, a request is made and return the JSON data.
    Otherwise, the data is considered as a domaine name. Nothing is done for
    this part yet.
    
    The domain syntax isn't tested but should be in the future.
    
    Signed-off-by: Matthieu Berjon <matthieu@berjon.net>

--- hdgnetwork/cli.py
+++ hdgnetwork/cli.py
@@ -1,18 +1,65 @@
 # -*- coding: utf-8 -*-
 
 """Console script for hdgnetwork."""
+import ipaddress
 import sys
+
+# 3rd party libraries
 import click
 
+from . import __version__
+from .whois import Whois
 
-@click.command()
-def main(args=None):
-    """Console script for hdgnetwork."""
-    click.echo("Replace this message by putting your code into "
-               "hdgnetwork.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
-    return 0
+
+def version_msg():
+    """ Returns the program version, location and python version
+    """
+    python_version = sys.version[:3]
+    message = 'hdg %(version)s (Python {})'
+    return message.format(python_version)
+
+
+@click.group(context_settings=dict(help_option_names=['-h', '--help']))
+@click.version_option(
+        __version__,
+        '-V',
+        '--version',
+        message=version_msg(),
+        help='Output the version of the application')
+
+
+@click.pass_context
+def main(ctx):
+    """HDG is a network tool suite
+
+    Here a better description
+    """
+    pass
+
+
+@main.command('whois')
+@click.argument('data', nargs=1)
+@click.pass_context
+def whois(ctx, data):
+    """Perform a whois request on a domain or IP
+
+    IPv4 and IPv6 are accepted.
+
+    Examples:\n
+        hdg whois 93.184.216.34\n
+        hdg whois example.net\n
+        hdg whois 2606:2800:220:1:248:1893:25c8:1946\n
+    """
+    whois = Whois()
+    try:
+        ipaddress.ip_address(data)
+        # data is an IP v4 or v6 address
+        result = whois.get_from_ip(data)
+        print(result)
+    except ValueError:
+        # data is a domain
+        click.echo('It is a domain')
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+    cli()  # pragma: no cover
