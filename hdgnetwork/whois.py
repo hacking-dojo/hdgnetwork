5ea08b9f6129923a202e23e025eb403d42b0be90
ben <benoit@systemd.info>
2018-08-15 23:13:57 +0200

    Improve error case

--- hdgnetwork/whois.py
+++ hdgnetwork/whois.py
@@ -15,19 +15,17 @@ class Whois():
         rdap_url_request = BASE_URL + str(ip_str)
         req = requests.get(rdap_url_request)
         output = json.loads(req.text)
+        owner = 'Not found'
+        addr = 'Not found'
 
-        if 'entities' not in output:
-            return None
+        if 'entities' in output:
+            first_vcard = output['entities'][0]['vcardArray'][1]
 
-        first_vcard = output['entities'][0]['vcardArray'][1]
-        owner = ''
-        addr = ''
-
-        for block in first_vcard:
-            if block[0] == 'fn':
-                owner = block[3]
-            if block[0] == 'adr':
-                addr = block[1]['label']
+            for block in first_vcard:
+                if block[0] == 'fn':
+                    owner = block[3]
+                if block[0] == 'adr':
+                    addr = block[1]['label']
 
         answer = dict()
         answer["owner"] = owner
